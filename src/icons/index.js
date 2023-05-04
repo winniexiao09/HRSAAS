0ffb5a17effd1ff21ea42b51189e7742c34056a7
涂俊涛 <booktu8@gmail.com>
2023-05-04 23:21:54 +0800

    上传图片

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
-// register globally
+// 在svg-icon全局组价中使用
 Vue.component('svg-icon', SvgIcon)
 
+// 1.读取所有.svg后缀的文件,导入到项目中
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
